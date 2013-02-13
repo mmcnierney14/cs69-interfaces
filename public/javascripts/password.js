4031bad65c8c9cb185e057ea775eebcc29cfadd0
Matthew McNierney <mmcnierney14@gmail.com>
2013-02-13 15:59:16 -0500

    errors check handles multiple errors

--- public/javascripts/password.js
+++ public/javascripts/password.js
@@ -1,17 +1,32 @@
 $(document).ready(function() {
   
   $('form#create-account').submit(function(event) {
-    var submission = $('form#create-account').serializeArray();
-    var error;
+    submission = $('form#create-account').serializeArray();
+    errors = [];
     
     // password and password confirmation don't match
-    if (submission[1].value != submission[2].value) {
-      error = "The password and the confirmation don't match";
+    if (submission[1].value != submission[2].value)
+      errors.push("The password and the confirmation don't match");
+    
+    // password isn't 8 characters
+    if (submission[1].value.length != 8)
+      errors.push("Password must be 8 characters in length");
+    
+    // display errors
+    if (errors.length > 0) {
+      $("#error").html("<strong>Oh snap!</strong> Something went wrong.<ul>");
+      
+      $.each(errors, function(index,value) {
+        $("#error").append("<li>" + value + "</li>");
+      });
+      
+      $("#error").append("</ul>").show();
     }
-    
-    if (error)
-      $("#error").html("<strong>Oh snap!</strong> " + error).show();
-    
+    else {
+      errors = [];
+      $("#error").hide();
+    }
+        
     event.preventDefault();
   });
   
